255c5a961c3e77c27d8917fea60b95c8c1a2aad9
Kamil <k.rostkowski@o2.pl>
2021-10-29 13:34:26 +0200

    4

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == "POST"    :
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            
